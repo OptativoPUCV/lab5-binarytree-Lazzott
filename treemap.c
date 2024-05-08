c51a96d1d61d0b3532f7226728d153c6345c8d55
Your Name <you@example.com>
2024-05-08 17:57:45 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -160,8 +160,6 @@ void removeNode(TreeMap * tree, TreeNode* node)
     }
     free(node->pair->key);
     free(node->pair->value);
-    free(node->pair);
-    free(node);
 }
 
 void eraseTreeMap(TreeMap * tree, void* key){
